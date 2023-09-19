16b2c0eecedcd3167bdd6d955b5d829df0b8c742
unknown <Administrator@HITESHKUMAR_LTP.DDN.UPES.AC.IN>
2023-09-19 09:16:38 +0530

    second commit

--- src/main/java/devops/b3/cicd/lab3/MyCalss.java
+++ src/main/java/devops/b3/cicd/lab3/MyCalss.java
@@ -5,6 +5,10 @@ public class MyCalss {
 	{
 		System.out.println("Hello GitHub Actions");
 	}
+	public void testMsg()
+	{
+		System.out.println("This is Test Message");
+	}
 
 	public static void main(String[] args) {		
 		MyCalss ob = new MyCalss();
