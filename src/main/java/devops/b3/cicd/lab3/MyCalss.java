f8b789f1d5de847aaf9cbadd3f5f41cfb44ae0ac
unknown <Administrator@HITESHKUMAR_LTP.DDN.UPES.AC.IN>
2023-09-19 08:53:58 +0530

    GitHub Actions

---
+++ src/main/java/devops/b3/cicd/lab3/MyCalss.java
@@ -0,0 +1,13 @@
+package devops.b3.cicd.lab3;
+
+public class MyCalss {
+	public void greeting()
+	{
+		System.out.println("Hello GitHub Actions");
+	}
+
+	public static void main(String[] args) {		
+		MyCalss ob = new MyCalss();
+		ob.greeting();
+	}
+}
